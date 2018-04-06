2e183e58d87beae8a7040bffe16ea7a096202f8f
tusshark <tusshark@chetu.com>
2018-04-06 17:09:21 +0530

    Just Start the application with Native Android

--- App1/MainActivity.cs
+++ App1/MainActivity.cs
@@ -1,16 +1,27 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
 
 namespace App1
 {
     [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private List<string> lstData;
+        private ListView myLisyView;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
+            // set your view from  the mainlayout resource
+            SetContentView(Resource.Layout.Main);
+            myLisyView = FindViewById<ListView>(Resource.Id.lstView);
+            lstData = new List<string>();
+            lstData.Add("First Item"); lstData.Add("Second Item"); lstData.Add("Third Item");
+            ArrayAdapter<string> objArrayAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, lstData);
+
+            myLisyView.Adapter = objArrayAdapter;
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
---
+++ App1/myListViewAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    class myListViewAdapter: BaseAdapter<string>
+    {
+        private List<string> objItems;
+        private Context objContext;
+
+        public myListViewAdapter(Context context, List<string> items)
+        {
+            objContext = context;
+            objItems = items;
+        }
+    }
+}
